865f4456711874acd7614942281c1967226c198e
ad222kr <ad222kr@student.lnu.se>
2015-04-06 23:01:48 +0200

    project created

---
+++ EndlessRunner/core/src/com/alexd/thegame/TheGame.java
@@ -0,0 +1,20 @@
+package com.alexd.thegame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class TheGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+}
