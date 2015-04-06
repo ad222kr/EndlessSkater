308aed512e0b680615a6fa3161a52a25bc244a17
ad222kr <ad222kr@student.lnu.se>
2015-04-06 23:05:15 +0200

    Changed TheGame to inherit from Game to support multiple screens

--- EndlessRunner/core/src/com/alexd/thegame/TheGame.java
+++ EndlessRunner/core/src/com/alexd/thegame/TheGame.java
@@ -1,13 +1,10 @@
 package com.alexd.thegame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class TheGame extends ApplicationAdapter {
-	
+
+public class TheGame extends Game {
+
 	@Override
 	public void create () {
 
