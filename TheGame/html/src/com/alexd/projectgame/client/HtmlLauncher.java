bc477bfefb26c6c01b0fe34088d59c87d07253ce
ad222kr <ad222kr@student.lnu.se>
2015-04-07 11:26:59 +0200

    Assets and some other folders dissapeared from android-proj so have generated a new one

---
+++ TheGame/html/src/com/alexd/projectgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.alexd.projectgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.alexd.projectgame.TheGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TheGame();
+        }
+}
