9704d46b98f65cc970b3098eba1506b5d62c779b
ad222kr <ad222kr@student.lnu.se>
2015-04-15 14:20:02 +0200

    Setup of test-enviroment, importet jUnit. Had some trouble but now it works

---
+++ TheGame/core/test/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest{
+    @Test
+    public void sample(){
+        assertEquals("Bananas", "Bananas");
+    }
+}
